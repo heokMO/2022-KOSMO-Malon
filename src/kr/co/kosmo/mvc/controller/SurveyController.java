9fff57c6b4ffca4b4dcf019ff69cd5941adf4ea0
Kwon <gurah1220@gmail.com>
2022-02-12 18:05:13 +0900

    test end

--- src/kr/co/kosmo/mvc/controller/SurveyController.java
+++ src/kr/co/kosmo/mvc/controller/SurveyController.java
@@ -1,5 +1,13 @@
 package kr.co.kosmo.mvc.controller;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.kosmo.mvc.dao.SurveyDaoInter;
 import kr.co.kosmo.mvc.dto.AnswerVO;
+import kr.co.kosmo.mvc.dto.SurveyResultVO;
 import kr.co.kosmo.mvc.dto.SurveyVO;
 
 @Controller
@@ -33,6 +42,14 @@ public class SurveyController {
 	
 	@RequestMapping(value = "/result")
 	public String result(Model m) {
+		List<SurveyResultVO> list = surveyDaoInter.getList();
+		List<String> keys = surveyDaoInter.getKeys();
+		Map<String, SurveyResultVO> result = new HashMap<>();
+		for(int i = 0; i < list.size(); i++) {
+			result.put(keys.get(i), list.get(i));
+		}
+		
+		m.addAttribute("result", result);
 		return "survey/result";
 	}
 	
--- src/kr/co/kosmo/mvc/dao/SurveyDaoInter.java
+++ src/kr/co/kosmo/mvc/dao/SurveyDaoInter.java
@@ -1,10 +1,15 @@
 package kr.co.kosmo.mvc.dao;
 
+import java.util.List;
+
 import kr.co.kosmo.mvc.dto.AnswerVO;
+import kr.co.kosmo.mvc.dto.SurveyResultVO;
 import kr.co.kosmo.mvc.dto.SurveyVO;
 
 
 public interface SurveyDaoInter {
 	public void submitSuvey(AnswerVO vo);
 	public SurveyVO getSuvey(int survey_idx);
+	public List<SurveyResultVO> getList();
+	public List<String> getKeys();
 }
